fab046ada24dc0abf78c3a5c407eff1c5b56b1c5
Din <diniskakov@gmail.com>
2020-08-24 16:36:49 +0200

    OOPgame improved

--- OOPgame.py
+++ OOPgame.py
@@ -1,7 +1,12 @@
 from tkinter import *
-import random
+from tkinter import messagebox
+from random import randint
+from random import *
 import time
 
+globalspeedp = 0
+globalspeedn = 0
+norp = ''
 
 class Ball():
 
@@ -11,7 +16,7 @@ class Ball():
         self.id = canvas.create_oval(10, 10, 25, 25, fill = color)
         self.canvas.move(self.id, 245, 100)
         starts = [-3, -2, -1, 1, 2, 3]
-        random.shuffle(starts)
+        shuffle(starts)
         self.x = starts[0]
         self.y = -3
         self.canvas_height = self.canvas.winfo_height()
@@ -27,14 +32,27 @@ class Ball():
         return False
 
     def draw(self):
+        global count, pointcount
         self.canvas.move(self.id, self.x, self.y)
         pos = self.canvas.coords(self.id)
         if pos[1] <= 0:
             self.y = 3
         if pos[3] >= self.canvas_height:
             self.hit_bottom = True
+            canvas.delete("all")
+            canvas.create_text(250, 180, text = "You lost", width = 100, font = "Verdana 14")
+            canvas.delete("all")
+            tk.mainloop()
+
         if self.hit_paddle(pos) == True:
-            self.y = -3
+            if norp == 'n':
+                self.y = globalspeedn
+            else:
+                self.y = globalspeedp * -1
+            count += 1
+            canvas.delete(pointcount)
+            pointcount = canvas.create_text(480, 50, text=count)
+
         if pos[0] <= 0:
             self.x = 3
         if pos[2] >= self.canvas_width:
@@ -60,10 +78,23 @@ class Paddle:
         if pos[2] >= self.canvas_width:
             self.x = 0
 
+
+
     def turn_left(self, evt):
-        self.x = -2
+        global globalspeedn, norp
+        notstarts = [-6, -5, -4, -3, -2]
+        randnum = randint(0, 4)
+        self.x = notstarts[randnum]
+        globalspeedn = self.x
+        norp = 'n'
+
     def turn_right(self, evt):
-        self.x = 2
+        global globalspeedp, norp
+        notstarts = [6, 5, 4, 3, 2]
+        randnum = randint(0, 4)
+        self.x = notstarts[randnum]
+        globalspeedp = self.x
+        norp = 'p'
 
 
 tk = Tk()
@@ -72,17 +103,22 @@ tk.resizable(0, 0)
 tk.wm_attributes("-topmost", 1)
 canvas = Canvas(tk, width = 500, height = 400, bd = 0, highlightthickness = 0)
 canvas.pack()
+count = 0
+pointcount = canvas.create_text(480, 50, text = count)
+
+
 tk.update()
 
+def funccall(event):
+    paddle = Paddle(canvas, 'blue')
+    ball = Ball(canvas, paddle, 'red')
 
-paddle = Paddle(canvas, 'blue')
-ball = Ball(canvas, paddle, 'red')
+    while 1:
+        ball.draw()
+        paddle.draw()
+        tk.update_idletasks()
+        tk.update()
+        time.sleep(0.01)
 
-while 1:
-    ball.draw()
-    paddle.draw()
-    tk.update_idletasks()
-    tk.update()
-    time.sleep(0.01)
-
+canvas.bind_all('<Button-1>', funccall)
 tk.mainloop()
